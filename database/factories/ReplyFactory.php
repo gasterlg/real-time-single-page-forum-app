b284b5f19b4887bb0cd3c3814e3adf8842b40ac4
gasterlg <luca@gril.li>
2020-04-07 10:14:45 +0800

    Factory and Seed

--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -3,10 +3,19 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model\Reply;
+use App\User;
+use App\Model\Question;
 use Faker\Generator as Faker;
 
 $factory->define(Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text,
+        'question_id' => function() {
+            return Question::all()->random();
+        },
+        'user_id' => function() {
+            return User::all()->random();
+        }
+
     ];
 });
